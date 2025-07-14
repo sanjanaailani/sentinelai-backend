c9874846ca5779988b4e9ce9a8c3ee1e0070c8ad
Sanjana Ailani <sanjanasmacbook@Sanjanas-MacBook-Air.local>
2025-07-14 17:42:59 +0530

    Fix analyze view to return JsonResponse

--- analyzer/views.py
+++ analyzer/views.py
@@ -1,6 +1,4 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from django.http import JsonResponse
 
-@api_view(['GET'])
 def analyze(request):
-    return Response({"message": "Analyze endpoint is working"})
+    return JsonResponse({"message": "Analyze endpoint is working"})
